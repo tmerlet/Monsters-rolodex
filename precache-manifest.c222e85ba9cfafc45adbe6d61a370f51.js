ee32fd7109aa266b5eac9a22c770c4c704243d6b
toby merlet <tmerlet@gmail.com>
2020-04-17 09:14:04 +0100

    Updates

---
+++ precache-manifest.c222e85ba9cfafc45adbe6d61a370f51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "523efcdaec0fd05e2506eb41230f0495",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c7cdcd5a525f2e6170d2",
+    "url": "/Monsters-rolodex/static/css/main.f82f0f1c.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/Monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7cdcd5a525f2e6170d2",
+    "url": "/Monsters-rolodex/static/js/main.96a77aa0.chunk.js"
+  },
+  {
+    "revision": "b7aacdd7dbffb780de32",
+    "url": "/Monsters-rolodex/static/js/runtime-main.bcfefaab.js"
+  }
+]);
